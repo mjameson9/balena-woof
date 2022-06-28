976913f09eeb4b3dc1cd82c3d896ea3422913ba1
Matt Jameson <mattjamesonit@gmail.com>
2022-06-28 15:06:26 -0700

    Added test sound, changed code to play it. Attempting to determine errorless playback failure.

--- woofer/woofer.py
+++ woofer/woofer.py
@@ -1,15 +1,19 @@
 import pygame
+
+# Confirm code is running
 print("Program didn't crash. Yay!")
 
 # Initialize pygame
-pygame.mixer.pre_init(44100, 16, 2, 4096) #frequency, size, channels, buffersize
 pygame.init()
 
 # Setup for sounds
+pygame.mixer.pre_init(44100, 16, 2, 4096) #frequency, size, channels, buffersize
 pygame.mixer.init()
   
 # Load all sound files
-outside_sound = pygame.mixer.Sound("/data/my_data/sounds/outside.wav")
+outside_sound = pygame.mixer.Sound("outside.wav")
+starwars_sound = pygame.mixer.Sound("StarWars60.wav")
 
 #Play Outside Sound
-outside_sound.play()
+# outside_sound.play()
+starwars_sound.play()
